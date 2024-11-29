42508bb38c309663143ce4b409c6371140621a7c
aadarsh <aadarshg087@gmail.com>
2024-11-29 16:27:36 +0530

    temp

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cooker-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use("/hello", (req, res) => {
+  res.send("Hello Guys");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Server is running at 3000");
+});
 
 export { app };
